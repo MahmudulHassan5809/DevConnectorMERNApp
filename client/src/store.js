a41437d521267112571da1614263bdbaab37a97b
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-09-28 02:20:08 +0600

    store change

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,24 @@ import rootReducer  from './reducers';
 const initialState = {};
 const middleware = [thunk]
 
-const store = createStore(
-	rootReducer,
-	initialState,
-	compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+if(process.env.NODE_ENV === 'production') {
+    const store = createStore(
+			rootReducer,
+			initialState,
+	);
+}else{
+	const store = createStore(
+			rootReducer,
+			initialState,
+			compose(applyMiddleware(...middleware),
+		    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+			)
 
-);
+	);
+}
+
+
+
 
 
 export default store;
